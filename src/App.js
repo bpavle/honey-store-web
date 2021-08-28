6fb4fb69bf0a4541b9513e03ff43ffeb584e9819
bpavle <bpavle0@gmail.com>
2021-08-29 00:57:03 +0200

    Added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
 import Header from './components/Header'
 import Menu from './components/Menu'
+import Products from './components/Products'
+import About from './components/About';
+import MyProfile from './components/MyProfile';
+import Orders from './components/Orders';
 function App() {
   return (<div className="App">
     <Header />
     <Menu />
+    <Route exact path="/home" component={Products} />
+    <Route exact path="/about-us" component={About} />
+    <Route exact path="/orders" component={Orders} />
+    <Route exact path="/my-profile" component={MyProfile} />
+
   </div>
   );
 }
