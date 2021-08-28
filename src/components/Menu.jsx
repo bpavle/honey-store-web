434bc573919c2973b330b8cdf5e6c27c885454ff
bpavle <bpavle0@gmail.com>
2021-08-29 00:57:58 +0200

    Tiny menu setup

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import styles from "./Menu.module.css";
+const menuItems = ["Home", "About-us", "Orders", "My-profile"];
 const Menu = (props) => {
-  return <div className={styles.Menu}>Menu</div>;
+  return (
+    <div className={styles.Menu}>
+      {menuItems.map((menuItem) => (
+        <div className={styles.menuButton}>
+          <Link to={menuItem} className={styles.menuButtonText}>
+            {" "}
+            {menuItem}{" "}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
