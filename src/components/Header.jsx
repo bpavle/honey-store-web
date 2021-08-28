4ae36e5a71cb3adc4d2012152f27d5814aff0c8f
bpavle <bpavle0@gmail.com>
2021-08-29 01:16:14 +0200

    Basic header formating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import styles from "./Header.module.css";
 const Header = (props) => {
-  return <div className={styles.Header}>Header info</div>;
+  return (
+    <div className={styles.Header}>
+      <div className={styles.Left}>Medenjaci</div>
+      <div className={styles.Right}>
+        <p>Контакт Пере Велимировића 8</p>
+        <p>E пошта: medenjaci@gmail.com </p>
+        <p>тел: 0641234568</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
