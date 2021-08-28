0b014807318ee5c03a1c4214fe6bdf8a83eae3e9
bpavle <bpavle0@gmail.com>
2021-08-29 01:45:14 +0200

    About page finished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,39 @@
+import React from "react";
+import styles from "./About.module.css";
 const About = () => {
-  return <div>About page</div>;
+  return (
+    <div className={styles.About}>
+      Пчеларска радња медењаци настала је 1999 године са циљем да људима у нашем
+      граду заслади живот, пружи здраве пчелиње производе попут меда, прополиса,
+      саћа... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+      sit amet molestie quam. Nunc consequat ex ac suscipit aliquet. Class
+      aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+      himenaeos. Aenean dolor felis, dictum at nibh ac, feugiat condimentum
+      augue. In suscipit imperdiet leo, eu maximus urna rhoncus in. Integer
+      blandit est a augue rhoncus, eu euismod nulla dictum. Nulla sed consequat
+      lacus. Vivamus id volutpat orci, eu pellentesque ante. In hac habitasse
+      platea dictumst. Morbi lectus enim, consequat sit amet enim in, luctus
+      viverra dui. Proin facilisis aliquam dui, at posuere lorem tempor at.
+      Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce congue
+      eu dui vel euismod. Etiam sagittis lorem quis ornare venenatis. Proin
+      porta ut nibh in consectetur. Vestibulum semper lacus sem, eget maximus
+      turpis mollis quis. Sed vehicula, ipsum a varius auctor, metus lacus
+      dictum nibh, et faucibus purus augue in risus. Mauris tincidunt sodales
+      dui in dictum. Ut pretium pulvinar urna. Duis vehicula tincidunt odio, in
+      sodales lorem ultrices in. Integer ut felis massa. Ut fermentum elit non
+      purus luctus placerat. Nunc ut magna dictum, ullamcorper eros at, accumsan
+      metus. Nunc eros dolor, luctus vitae enim ut, ornare maximus leo. Sed
+      rutrum magna ut purus viverra facilisis. Suspendisse bibendum eget enim at
+      dapibus. Orci varius natoque penatibus et magnis dis parturient montes,
+      nascetur ridiculus mus. Etiam molestie posuere mi in pulvinar. Orci varius
+      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Morbi leo turpis, aliquam vitae hendrerit ac, ullamcorper sed mi.
+      Curabitur dapibus vehicula libero, sit amet dignissim sapien iaculis non.
+      Aliquam aliquam facilisis pulvinar. Maecenas vitae malesuada magna. Nunc
+      in purus eleifend, consequat erat eget, accumsan justo. Proin arcu turpis,
+      tempus et est finibus, laoreet tincidunt odio.
+    </div>
+  );
 };
 
 export default About;
