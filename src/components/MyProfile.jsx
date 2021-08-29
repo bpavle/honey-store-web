6bbd2b10aa9c686e226b3a1f5a311d2f6e5b3de6
bpavle <bpavle0@gmail.com>
2021-08-29 02:11:51 +0200

    Table form

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,5 +1,66 @@
+import React from "react";
+import styles from "./MyProfile.module.css";
 const MyProfile = () => {
-  return <div>My profile page</div>;
+  return (
+    <div className={styles.MyProfile}>
+      <table>
+        <tr>
+          <td>Ime</td>
+          <td>
+            {" "}
+            <input type="text"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Prezime</td>
+          <td>
+            {" "}
+            <input type="text"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Kontakt telefon</td>
+          <td>
+            {" "}
+            <input type="text"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Adresa</td>
+          <td>
+            {" "}
+            <input type="text"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Korisnicko ime</td>
+          <td>
+            {" "}
+            <input type="text"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Lozinka</td>
+          <td>
+            {" "}
+            <input type="password"></input>
+          </td>
+        </tr>
+        <tr>
+          <td>Ponovi lozinku</td>
+          <td>
+            {" "}
+            <input type="password"></input>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <button>Sacuvaj</button>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default MyProfile;
