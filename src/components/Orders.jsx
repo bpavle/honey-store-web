5b7023f8b7839c64a9bd3eaf159c34b8fa7bcd85
bpavle <bpavle0@gmail.com>
2021-08-29 02:38:41 +0200

    Added table for orders

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,5 +1,32 @@
+import React from "react";
+import styles from "./Orders.module.css";
 const Orders = () => {
-  return <div>Orders page</div>;
+  return (
+    <div className={styles.Orders}>
+      <table>
+        <tr>
+          <th>ID porudzbine</th>
+          <th>Ime</th>
+          <th>Prezime</th>
+          <th>Telefon</th>
+          <th>Adresa</th>
+          <th>Korisnicko ime</th>
+          <th>Odobri</th>
+          <th>Odbij</th>
+        </tr>
+        <tr>
+          <td>33182020</td>
+          <td>Pera</td>
+          <td>Peric</td>
+          <td>0641234567</td>
+          <td>Goricka 82 a</td>
+          <td>ppera99</td>
+          <td>D</td>
+          <td>O</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Orders;
