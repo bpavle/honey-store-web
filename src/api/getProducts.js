0e1491a5cbb561d2d6247a0a23fb112ab0ca2ec5
bpavle <bpavle0@gmail.com>
2021-09-05 15:34:47 +0200

    Added more mock products

---
+++ src/api/getProducts.js
@@ -0,0 +1,85 @@
+import tegla from "../images/tegla.png"
+import medenjaci from "../images/medenjaci.png";
+
+const products = [{
+    id: 1,
+    image: tegla,
+    label: "Neki tekst"
+},
+{
+    id: 2,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 3,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 4,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 5,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 6,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 7,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 8,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 9,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 10,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 11,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 12,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+    ,
+{
+    id: 13,
+    image: medenjaci,
+    label: "Neki tekst"
+}
+
+]
+
+export function getProducts() {
+    return products
+}
