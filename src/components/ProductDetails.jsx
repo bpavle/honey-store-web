13b887771d9c3ca518d35015080975dea0cdda8e
bpavle <bpavle0@gmail.com>
2021-09-05 23:53:49 +0200

    Added product details component

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
 import styles from "./ProductDetails.module.css";
-const ProductDetails = () => {
-  return <div className={styles.container}>Detalji o proizvodu</div>;
+const ProductDetails = ({ label, image, price, info }) => {
+  let [amount, setAmount] = useState(1);
+  const onClickHandler = (e) => {
+    e.stopPropagation();
+  };
+  return (
+    <div className={styles.container} onClick={onClickHandler}>
+      <div className={styles.left}>
+        <img src={image} />
+      </div>
+      <div className={styles.right}>
+        <table className={styles.table}>
+          <h3>{label}</h3>
+
+          <tr>
+            <td>
+              <p>{info}</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <bold>{price} din</bold>
+            </td>
+          </tr>
+          <tr>
+            Amount
+            <input
+              type="number"
+              style={{ width: "30px", fontSize: 18 }}
+              defaultValue={amount}
+            />
+          </tr>
+          <button>Add to basket</button>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
