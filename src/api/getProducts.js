73ecbb859fc26a6ca87c3cbc6b4f32d733430da7
bpavle <bpavle0@gmail.com>
2021-09-05 21:09:43 +0200

    Added more info to fake api

--- src/api/getProducts.js
+++ src/api/getProducts.js
@@ -4,78 +4,105 @@ import medenjaci from "../images/medenjaci.png";
 const products = [{
     id: 1,
     image: tegla,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 },
 {
     id: 2,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 3,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 4,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 5,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 6,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 7,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 8,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 9,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 10,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 11,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 12,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100
 }
     ,
 {
     id: 13,
     image: medenjaci,
-    label: "Neki tekst"
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+
 }
 
 ]
