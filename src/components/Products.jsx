46761d9baebab65101a8936f5dfb190d0c10eedd
bpavle <bpavle0@gmail.com>
2021-09-05 15:41:10 +0200

    Added jsx table generator

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Products.module.css";
+import Product from "./Product";
+import tegla from "./../images/tegla.png";
+import { getProducts } from "./../api/getProducts.js";
 const Products = () => {
-  return <div className={styles.container}>Just some products for test</div>;
-};
+  let [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts(getProducts());
+    // console.log("Products" + JSON.stringify(products));
+  });
+  let table_data = products.map(function (product) {
+    return (
+      <td>
+        <Product label={product.label} image={product.image} />
+      </td>
+    );
+  });
+  const createTable = (elements, number_of_columns) => {
+    let data = [];
+    let rows = [];
+    for (let i = 0; i < elements.length; i++) {
+      data.push(elements[i]);
 
+      if ((i + 1) % number_of_columns == 0 || i + 1 == elements.length) {
+        let row = <tr>{data}</tr>;
+        data = [];
+        rows.push(row);
+      }
+    }
+    return <table>{rows}</table>;
+  };
+  const table = createTable(table_data, 3);
+  return <div className={styles.container}>{table}</div>;
+};
 export default Products;
