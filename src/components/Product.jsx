57eebffd6f059bc58b29ed5700fbfc437dc9a6c8
bpavle <bpavle0@gmail.com>
2021-09-05 15:41:24 +0200

    Some product ajustment

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from "./Products.module.css";
+import styles from "./Product.module.css";
 const Product = (props) => {
   return (
-    <div className={styles.product}>
+    <div className={styles.container}>
       <img src={props.image} alt="picture placeholder" />
       <div className={styles.label}>{props.label}</div>
     </div>
