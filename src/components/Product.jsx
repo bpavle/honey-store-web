62f2218c8f56e20710e402ff2f2f001179e331ff
bpavle <bpavle0@gmail.com>
2021-09-05 19:48:22 +0200

    Basic event handler

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./Product.module.css";
 const Product = (props) => {
+  const onClickHandler = () => {
+    console.log("Clicked" + props.id);
+  };
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={onClickHandler}>
       <img src={props.image} alt="picture placeholder" />
       <div className={styles.label}>{props.label}</div>
     </div>
