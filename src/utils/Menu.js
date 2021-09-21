8fbd7568c156033b478187f0e75607b1bd44d18e
bpavle <bpavle0@gmail.com>
2021-09-21 22:14:21 +0200

    Added right property

--- src/utils/Menu.js
+++ src/utils/Menu.js
@@ -15,61 +15,79 @@ const MenuItems = {
         label: "Products",
         component: Products,
         path: '/home',
-        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER']
+        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER'],
+        right: false
     },
     about: {
         label: "About",
         component: About,
         path: '/about-us',
-        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER']
+        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER'],
+        right: false
     },
     orders: {
         label: "Orders",
         component: Orders,
         path: '/orders',
-        roles: ['ROLE_ADMIN', 'ROLE_SELLER']
+        roles: ['ROLE_ADMIN', 'ROLE_SELLER'],
+        right: false
     },
     myProfile: {
         label: "My Profile",
         component: MyProfile,
         path: '/my-profile',
-        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_SELLER']
+        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_SELLER'],
+        right: false
     },
     employees: {
         label: "Employees",
         component: Employees,
         path: '/employees',
-        roles: ['ROLE_ADMIN']
+        roles: ['ROLE_ADMIN'],
+        right: false
     },
     addEmployee: {
         label: "Add Employee",
         component: AddEmployee,
         path: '/add-employee',
-        roles: ['ROLE_ADMIN']
+        roles: ['ROLE_ADMIN'],
+        right: false
     },
     contact: {
         label: "Contact",
         component: Contact,
         path: '/contact',
-        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER']
+        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_VISITOR', 'ROLE_SELLER'],
+        right: false
     },
     login: {
         label: "Login",
         component: Login,
         path: '/login',
-        roles: ['ROLE_VISITOR']
+        roles: ['ROLE_VISITOR'],
+        right: false
     },
     registration: {
         label: "Registration",
         component: Registration,
         path: '/registration',
-        roles: ['ROLE_VISITOR']
+        roles: ['ROLE_VISITOR'],
+        right: false
     },
     cart: {
         label: "Cart",
         component: Cart,
         path: '/cart',
-        roles: ['ROLE_CUSTOMER', 'ROLE_VISITOR']
+        roles: ['ROLE_CUSTOMER', 'ROLE_VISITOR'],
+        right: true
+    },
+    logout: {
+        label: "Logout",
+        component: null,
+        path: '/logout',
+        roles: ['ROLE_CUSTOMER', 'ROLE_ADMIN', 'ROLE_SELLER'],
+        right: false,
+
     }
 };
 
