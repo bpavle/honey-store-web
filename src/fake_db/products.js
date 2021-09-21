072ed2ba189e36988ecf7f083ee215c0a61401a1
bpavle <bpavle0@gmail.com>
2021-09-21 19:27:04 +0200

    Fake db

---
+++ src/fake_db/products.js
@@ -0,0 +1,122 @@
+import tegla from "../images/tegla.png"
+import medenjaci from "../images/medenjaci.png";
+let products = [{
+    id: 1,
+    image: tegla,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+},
+{
+    id: 2,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 3,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 4,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 5,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 6,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 7,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 8,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 9,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 10,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 11,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 12,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din"
+}
+    ,
+{
+    id: 13,
+    image: medenjaci,
+    label: "Neki tekst",
+    info: "Neki duzi info o ovom proizvodu koji se vidi kada se klikne",
+    price: 100,
+    currency: "din",
+
+}
+
+]
+
+export default products
---
+++ src/fake_db/users.js
@@ -0,0 +1,35 @@
+let users = [{
+    id: 1,
+    role: "ROLE_ADMIN",
+    name: "Pavle",
+    surname: "Bradic",
+    email: "mail@mail.com",
+    password: "password",
+    phone: "0688443165",
+    address: "Ulica 16 sprat 2",
+
+},
+{
+    id: 2,
+    role: "ROLE_VISITOR",
+    name: "Pera",
+    surname: "Peric",
+    email: "kupac@mail.com",
+    password: "password",
+    phone: "02342342345",
+    address: "Ulica 122 sprat 2",
+
+},
+{
+    id: 3,
+    role: "ROLE_ADMIN",
+    name: "Radoje",
+    surname: "Radovic",
+    email: "radnik@mail.com",
+    password: "password",
+    phone: "23849237432",
+    address: "Ulica 3216 sprat 3",
+
+}]
+
+export default users
