514ef8b1dcabfb8b2f412a6e94d1ad6bef9fbd7d
bpavle <bpavle0@gmail.com>
2021-09-21 21:27:24 +0200

    Only for development: Show role in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Header.module.css";
+import { UserContext } from "./Contexts/UserContext";
 const Header = (props) => {
+  const [user, setUser] = useContext(UserContext);
   return (
     <div className={styles.Header}>
       <div className={styles.Left}>Medenjaci</div>
@@ -8,6 +10,7 @@ const Header = (props) => {
         <p>Контакт Пере Велимировића 8</p>
         <p>E пошта: medenjaci@gmail.com </p>
         <p>тел: 0641234568</p>
+        {user.role}
       </div>
     </div>
   );
