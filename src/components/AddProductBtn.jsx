ea727cb16f0b5976a02d50aa4023ce7573f04831
bpavle <bpavle0@gmail.com>
2021-09-22 22:56:22 +0200

    Hide add product button for unauthorized users

--- src/components/AddProductBtn.jsx
+++ src/components/AddProductBtn.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import styles from "./AddProductBtn.module.css";
 import AddProduct from "./AddProduct";
+import { UserContext } from "./Contexts/UserContext";
 const AddProductBtn = (props) => {
+  let [user, setUser] = useContext(UserContext);
   let [isClicked, setIsClicked] = useState(props.clicked);
   const onClickHandler = (e) => {
     e.stopPropagation();
@@ -11,19 +13,20 @@ const AddProductBtn = (props) => {
   const clear = () => {
     setIsClicked(false);
   };
-
-  return (
-    <>
-      {isClicked && (
-        <div className={styles.clear} onClick={clear}>
-          <AddProduct />
-        </div>
-      )}
-      <button className={styles.button} onClick={onClickHandler}>
-        +
-      </button>
-    </>
-  );
+  if (user.role === "ROLE_ADMIN" || user.role === "ROLE_SELLER") {
+    return (
+      <>
+        {isClicked && (
+          <div className={styles.clear} onClick={clear}>
+            <AddProduct />
+          </div>
+        )}
+        <button className={styles.button} onClick={onClickHandler}>
+          +
+        </button>
+      </>
+    );
+  } else return null;
 };
 
 export default AddProductBtn;
