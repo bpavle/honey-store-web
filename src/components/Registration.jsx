bcc5f9cd291c5e006dcbd776fd2adcd183be764d
bpavle <bpavle0@gmail.com>
2021-09-22 22:50:03 +0200

    Register user functionality

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 import styles from "./Registration.module.css";
 import Button from "./Common/Button";
+import { addUser } from "../api/usersServices";
 const Registration = () => {
+  const registerUser = () => {
+    let newUser = {
+      role: "ROLE_CUSTOMER",
+      name: document.getElementById("name").value,
+      surname: document.getElementById("surname").value,
+      phone: document.getElementById("phone").value,
+      address: document.getElementById("address").value,
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      "confirm-password": document.getElementById("confirm-password").value,
+    };
+    newUser.password === newUser["confirm-password"]
+      ? addUser(newUser)
+      : alert("Passwords do not match");
+  };
   return (
     <div className={styles.Registration}>
       <table className={styles.table}>
@@ -10,54 +26,54 @@ const Registration = () => {
             <td>Ime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input id="name" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Prezime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input id="surname" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Kontakt telefon</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input id="phone" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Adresa</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input id="address" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Korisnicko ime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input id="email" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Lozinka</td>
             <td>
               {" "}
-              <input type="password"></input>
+              <input id="password" type="password"></input>
             </td>
           </tr>
           <tr>
             <td>Ponovi lozinku</td>
             <td>
               {" "}
-              <input type="password"></input>
+              <input id="confirm-password" type="password"></input>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <Button>Register</Button>
+              <Button onClick={registerUser}>Register</Button>
             </td>
           </tr>
         </tbody>
