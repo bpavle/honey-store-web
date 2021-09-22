2210be1215f430e4e3b8f6bb93e7cb1ab8db7263
bpavle <bpavle0@gmail.com>
2021-09-22 16:57:17 +0200

    Remove console log

--- src/utils/PrivateRoute.jsx
+++ src/utils/PrivateRoute.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect } from "react";
 import { UserContext } from "./../components/Contexts/UserContext";
 import { Route, Redirect } from "react-router-dom";
 const PrivateRoute = ({ component, roles, ...rest }) => {
-  console.log(`Private route for ${rest.path} Roles :${roles}`);
   let [user, setUser] = useContext(UserContext);
   const isAuthenticated = user.isLoggedIn;
   return (
