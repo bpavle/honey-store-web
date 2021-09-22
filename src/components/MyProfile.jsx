75dbe9eccb492d724410cabb8c0ec73beea3cb6f
bpavle <bpavle0@gmail.com>
2021-09-22 22:49:53 +0200

    Add update user functionality

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import styles from "./MyProfile.module.css";
 import Button from "./Common/Button";
+import { updateUser, getUserById } from "../api/usersServices";
+import { UserContext } from "./Contexts/UserContext";
 const MyProfile = () => {
+  let [user, setUser] = useContext(UserContext);
+  console.log(user);
+  console.log(getUserById(user.id));
+  const changeInfo = () => {
+    let updatedUser = {
+      role: user.role,
+      name: document.getElementById("name").value
+        ? document.getElementById("name").value
+        : user.name,
+      surname: document.getElementById("surname").value
+        ? document.getElementById("name").value
+        : user.surname,
+      phone: document.getElementById("phone").value
+        ? document.getElementById("name").value
+        : user.phone,
+      address: document.getElementById("address").value
+        ? document.getElementById("name").value
+        : user.address,
+      email: document.getElementById("email").value
+        ? document.getElementById("name").value
+        : user.email,
+      password: document.getElementById("password").value,
+      "confirm-password": document.getElementById("confirm-password").value,
+    };
+    updatedUser.password === updatedUser["confirm-password"]
+      ? updateUser({ id: user.id, ...updatedUser }) &&
+        setUser({ ...getUserById(user.id), isLoggedIn: true })
+      : alert("Passwords do not match");
+  };
   return (
     <div className={styles.MyProfile}>
       <table className={styles.table}>
@@ -10,54 +41,66 @@ const MyProfile = () => {
             <td>Ime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input placeholder={user.name} id="name" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Prezime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input
+                placeholder={user.surname}
+                id="surname"
+                type="text"
+              ></input>
             </td>
           </tr>
           <tr>
             <td>Kontakt telefon</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input placeholder={user.phone} id="phone" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Adresa</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input
+                placeholder={user.address}
+                id="address"
+                type="text"
+              ></input>
             </td>
           </tr>
           <tr>
             <td>Korisnicko ime</td>
             <td>
               {" "}
-              <input type="text"></input>
+              <input placeholder={user.email} id="email" type="text"></input>
             </td>
           </tr>
           <tr>
             <td>Lozinka</td>
             <td>
               {" "}
-              <input type="password"></input>
+              <input
+                placeholder={user.password}
+                id="password"
+                type="password"
+              ></input>
             </td>
           </tr>
           <tr>
             <td>Ponovi lozinku</td>
             <td>
               {" "}
-              <input type="password"></input>
+              <input id="confirm-password" type="password"></input>
             </td>
           </tr>
           <tr>
             <td colSpan="2">
-              <Button>Sacuvaj</Button>
+              <Button onClick={changeInfo}>Sacuvaj</Button>
             </td>
           </tr>
         </tbody>
