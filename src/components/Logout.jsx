e6e297c482899553289128ae7a754247cd8d86f9
bpavle <bpavle0@gmail.com>
2021-09-22 16:57:09 +0200

    Redirect to home page after component is rerendered.

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,14 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import { UserContext } from "./Contexts/UserContext";
 const Logout = () => {
   const history = useHistory();
   let [user, setUser] = useContext(UserContext);
+
+  useEffect(() => {
+    history.push("/home");
+  });
+
   console.log("Running logout");
 
   setUser({ role: "ROLE_VISITOR", isLoggedIn: false });
-  history.push("/home");
+
   return <div>test</div>;
 };
 
