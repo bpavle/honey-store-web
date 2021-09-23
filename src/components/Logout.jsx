108d7c1f5e5425cf1f7a5258f7194e9f10c47f1e
bpavle <bpavle0@gmail.com>
2021-09-23 02:35:26 +0200

    Empty cart on logout

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -2,9 +2,12 @@ import React, { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import { UserContext } from "./Contexts/UserContext";
+import { CartContext } from "./Contexts/CartContext";
+
 const Logout = () => {
   const history = useHistory();
   let [user, setUser] = useContext(UserContext);
+  let [cart, setCart] = useContext(CartContext);
 
   useEffect(() => {
     history.push("/home");
@@ -13,7 +16,7 @@ const Logout = () => {
   console.log("Running logout");
 
   setUser({ role: "ROLE_VISITOR", isLoggedIn: false });
-
+  setCart([]);
   return <div>test</div>;
 };
 
